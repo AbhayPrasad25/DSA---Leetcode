96ceee6dd56cd54835dd553a01aa01dcf916f25c
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-05-01 17:05:55 +0530

    Time: 2 ms (95.13%), Space: 134.1 MB (29.57%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        // My First priority should be checking consecutive indices since it asks to solve in O(n) time
+        // Neither consecuitve indices worked nether brute worked due to constrains so i greedily selected the fisrt and second so that the at every index either thr first the smallest is selected and in next iteration the second smallest is selected and finally we get a element which is far greater than the first and second an we return true
         int first = Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] < second && nums[i] < first){
+            if(nums[i] <= first){
                 first = nums[i];
             }
-            else if(nums[i] > first && nums[i] < second){
+            else if(nums[i] <= second){
                 second = nums[i];
             }
-            else if(nums[i] > second){
+            else{
+                //The next element is greater we return true
                 return true;
             }
-
         }
         return false;
     }
