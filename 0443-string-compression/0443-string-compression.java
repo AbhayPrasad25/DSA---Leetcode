76c5f12a4d01773a6fadd765a0b9324f76333e9f
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-05-01 11:10:58 +0530

    Time: 1 ms (99.56%), Space: 44 MB (66.59%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,33 +1,30 @@
 class Solution {
     public int compress(char[] chars) {
-        //First With Extra Space
-        StringBuilder str = new StringBuilder();
+        //Without Extra Space
+        int indexPtr = 0;
         char c1 = chars[0];
         int count = 1;
         for (int i = 1; i < chars.length; i++) {
             char c = chars[i];
             if (c1 != c) {
-                str.append(c1);
-                if(count != 1){
-                    str.append(count);
+                chars[indexPtr++] = c1;
+                if (count != 1) {
+                    for (char ch : String.valueOf(count).toCharArray()) {
+                        chars[indexPtr++] = ch;
+                    }
                 }
-                count = 1;
                 c1 = c;
+                count = 1;
             } else {
                 count++;
             }
         }
-        if(count == 1){
-            str.append(c1);
+        chars[indexPtr++] = c1;
+        if (count != 1) {
+            for (char ch : String.valueOf(count).toCharArray()) {
+                chars[indexPtr++] = ch;
+            }
         }
-        else{
-            str.append(c1);
-            str.append(count);
-        }
-        String s = str.toString();
-        for(int i = 0; i < s.length(); i++){
-            chars[i] = s.charAt(i);
-        }
-        return s.length();
+        return indexPtr;
     }
 }
