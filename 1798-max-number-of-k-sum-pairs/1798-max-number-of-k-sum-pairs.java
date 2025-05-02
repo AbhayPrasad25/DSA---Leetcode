38b00901bc8f9e9db7f0795f32d292e2ec5ad023
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-05-02 12:07:18 +0530

    Time: 18 ms (92.91%), Space: 58 MB (16.92%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -1,16 +1,22 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        // Optimal Approach using HashMap and finding the complement
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // Better Approach sorting and two pointers
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
         int operation = 0;
-        for(int num : nums){
-            int comp = k - num;
-            if(map.getOrDefault(comp, 0) > 0){
-                operation++;
-                map.put(comp , map.getOrDefault(comp , 0) - 1);
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            if(sum == k){
+                operation += 1;
+                left++;
+                right--;
+            }
+            else if(sum < k){
+                left++;
             }
             else{
-                map.put(num , map.getOrDefault(num , 0) + 1);
+                right--;
             }
         }
         return operation;
