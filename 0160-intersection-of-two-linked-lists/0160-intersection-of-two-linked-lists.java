e953bce684139c334fcd079d6a72aaa0f9e696b0
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-05-02 15:19:52 +0530

    Time: 1 ms (99.93%), Space: 48.6 MB (68.8%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,19 +11,32 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        //Better Approach which takes O(1) space
-        ListNode tempA = headA;
-        // Now in this approach every time we move the tempA pointer one step we check if any of the nodes in B have the sameReferecne value or not 
-        while(tempA != null){
-            ListNode tempB = headB;
-            while(tempB != null){
-                if(tempA == tempB){
-                    return tempA;
-                }
-                tempB = tempB.next;
-            }
-            tempA = tempA.next;
+        // Better Approach 
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        int len1 = 0 , len2  = 0;
+        while(temp1 != null){
+            len1++;
+            temp1 = temp1.next;
         }
-        return null;
+        while(temp2 != null){
+            len2++;
+            temp2 = temp2.next;
+        }
+        if(len1 > len2){
+            return collision(headA, headB, len1 - len2);
+        }
+        return collision(headB, headA, len2 - len1);
+    }
+    private ListNode collision(ListNode large, ListNode small, int dif){
+        while(dif > 0){
+            large = large.next;
+            dif--;
+        }
+        while(large != small){
+            large = large.next;
+            small = small.next;
+        }
+        return large;
     }
 }
