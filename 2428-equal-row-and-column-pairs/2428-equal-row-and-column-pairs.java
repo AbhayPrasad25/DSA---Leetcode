a351d620591bbe1df0a42d6ccc05c23d9566da1a
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-05-10 00:55:57 +0530

    Time: 20 ms (82.18%), Space: 50.4 MB (57.33%) - LeetHub

---
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        HashMap<List<Integer>, Integer> map = new HashMap<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i = 0; i < m; i++){
+            List<Integer> l = new ArrayList<>();
+            for(int j = 0; j < n; j++){
+                l.add(grid[i][j]);
+            }
+            map.put(l , map.getOrDefault(l , 0) + 1);
+        }
+        // Now itrating column wise and checking the number of equal pairs and column
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            List<Integer> l = new ArrayList<>();
+            for(int j = 0; j < m; j++){
+                l.add(grid[j][i]);
+            }
+            if(map.containsKey(l)){
+                count += map.get(l);
+            }
+        }
+        return count;
+    }
+}
