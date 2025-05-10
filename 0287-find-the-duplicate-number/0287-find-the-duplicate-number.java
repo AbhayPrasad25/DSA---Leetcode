d7e15c0e08ae5a1ebd01b5e09042c0e0868a176d
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-05-11 00:47:17 +0530

    Time: 4 ms (88.94%), Space: 58.3 MB (48.71%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,18 +2,19 @@ class Solution {
     public int findDuplicate(int[] nums) {
         //Brute force - iterate the array for each element and check if contains the same element
         //better approach - sort the array and check if the any of 2 consecutive pairs are same 
-        //Optimal approach - use xor operation to find the repeated element 
-        HashSet<Integer> set = new HashSet<>();
-        int repeat = 0;
-        for(int a :nums){
-            if(!set.contains(a)){
-                set.add(a);
-            }
-            else{
-                repeat = a;
-                break;
-            }
+        //Optimal approach - using floyds tortoise and hare algorithm
+        int fast = nums[0];
+        int slow = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        // now we reset one pointer to starting point
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return repeat;
+        return slow;
     }
 }
