0e24ef60965b5163863b58cbdfa8fca94425c20a
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-05-14 10:49:34 +0530

    Time: 45 ms (82.54%), Space: 64.7 MB (86.91%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -8,20 +8,20 @@ class Solution {
         return successArray;
     }
     private int leastsuc(int spell, int[] potions, long success){
-        int count = 0;
+        int index = potions.length;
         int left = 0;
         int right = potions.length - 1;
         while(left <= right){
             int mid = left + (right - left)/2;
             long power =  1L * spell * potions[mid];
             if(power >= success){
-                count += (right - mid + 1);
+                index = mid;
                 right = mid - 1;
             }
             else{
                 left = mid + 1;
             }
         }
-        return count;
+        return potions.length - index;
     }
 }
