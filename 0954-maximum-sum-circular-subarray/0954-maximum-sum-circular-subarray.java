ecbfe1e281903d5f18fa87e156949687d6309768
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-05-17 00:30:02 +0530

    Time: 7 ms (34.71%), Space: 49 MB (88.72%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        int curSum = 0 , totalSum = 0, minSum = 0;
-        for(int num : nums){
-            curSum = Math.max(num , curSum + num);
-            minSum = Math.min(num , minSum + num);
-            totalSum += num;
-            max = Math.max(max , curSum);
+        int max = nums[0];
+        int min = nums[0];
+        int maxSum = nums[0] , totalSum = nums[0], minSum = nums[0];
+        for(int i = 1; i< nums.length; i++){
+            maxSum = Math.max(nums[i] , maxSum + nums[i]);
+            minSum = Math.min(nums[i] , minSum + nums[i]);
+            totalSum += nums[i];
+            max = Math.max(max , maxSum);
             min = Math.min(min , minSum);
         }
         // Since both are same means the whole array is full of neg values
