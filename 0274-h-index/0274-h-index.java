8db1f6cb271ae17a0199f15e122dfeab1d567d26
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-06-01 01:51:10 +0530

    Time: 2 ms (69.7%), Space: 41.5 MB (81.06%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int index = 0;
+        for(int i = citations.length - 1; i >= 0; i--){
+            if(citations[i] >= citations.length - i){
+                index = citations.length - i;
+            }
+        }
+        return index;
+    }
+}
