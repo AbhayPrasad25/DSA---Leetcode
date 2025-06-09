2687611cf6ab5ab06cff8c7c9ea1f43ac0d7d788
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-06-09 17:18:21 +0530

    Time: 1 ms (51.97%), Space: 42.8 MB (64.54%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,23 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> subset = new ArrayList<>();
-        possible(0, ans, subset, nums);
+        List<Integer> res = new ArrayList<>();
+        subset(0, nums, ans, res);
         return ans;
     }
-    private void possible(int i, List<List<Integer>> ans, List<Integer> subset, int[] nums){
-        //Base case whenver the index crosses the length of the array we append the list in the ans list
-        if(i >= nums.length){
-            ans.add(new ArrayList<>(subset));
+    private void subset(int index, int[] nums, List<List<Integer>> ans, List<Integer> res){
+        if(index >= nums.length){
+            ans.add(new ArrayList<>(res));
             return;
         }
-        // we need to handle the cases when we add the first element
-        subset.add(nums[i]);
-        //move to the next index
-        possible(i+1, ans, subset, nums);
-        //when we have added all the elements in the arraylist we need to remove one at a time and move the next index
-        subset.remove(subset.size() - 1);
-        //again move to next index to find the possible cases
-        possible(i+1, ans, subset, nums);
+        res.add(nums[index]);
+        subset(index + 1, nums, ans, res);
+        res.remove(res.size() - 1);
+        subset(index + 1, nums, ans, res);
     }
 }
