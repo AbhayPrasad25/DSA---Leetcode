b6fb62e42c72550a1b27925b9a09636b1816972f
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-06-16 16:16:13 +0530

    Time: 2 ms (99.65%), Space: 43.2 MB (86.25%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,32 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        boolean[] used = new boolean[candidates.length];
         Arrays.sort(candidates);
-        HashSet<List<Integer>> set = new HashSet<>();
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
-        backtrack(candidates, target,0,0, res, ans , used);
+        backtrack(candidates, target, 0, 0, res, ans);
         return res;
     }
-    public void backtrack(int[] nums, int target, int sum, int index, List<List<Integer>> res , List<Integer> ans, boolean[] used){
-        if(sum == target){
+
+    public void backtrack(int[] nums, int target, int sum, int index, List<List<Integer>> res, List<Integer> ans) {
+        if (sum == target) {
             res.add(new ArrayList<>(ans));
             return;
         }
-        for(int i = index; i < nums.length; i++){
-            if(sum + nums[i] > target){
+        for (int i = index; i < nums.length; i++) {
+            if (sum + nums[i] > target) {
                 break;
             }
-            if(i > index && nums[i - 1] == nums[i]){
+            if (i > index && nums[i - 1] == nums[i]) {
                 continue;
             }
-            if(used[i] == false){
-                ans.add(nums[i]);
-                used[i] = true;
-                backtrack(nums, target, sum + nums[i], i + 1, res, ans, used);
-                ans.remove(ans.size() - 1);
-                used[i] = false;
-            }
+            ans.add(nums[i]);
+            backtrack(nums, target, sum + nums[i], i + 1, res, ans);
+            ans.remove(ans.size() - 1);
         }
     }
 }
