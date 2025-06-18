6688d5b541add22655004ef413d5f3874285ff8e
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-06-18 15:03:23 +0530

    Time: 6 ms (34.15%), Space: 44.8 MB (10.23%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, res, s, 0);
+        return res;
+    }
+    public void backtrack(List<String> ans, List<String> res, String s, int index){
+        if(index == s.length() && ans.size() == 4){
+            String st = String.join(".", ans);
+            res.add(st);
+            return;
+        }
+        for(int end = 1; end <= 3 && end + index <= s.length(); end++){
+            int num = Integer.parseInt(s.substring(index , index + end));
+            if( num <= 255){
+                if(s.charAt(index) == '0' && end == 1 || s.charAt(index) != '0'){
+                    ans.add(s.substring(index, index + end));
+                    backtrack(ans, res, s, index + end);
+                    ans.remove(ans.size() - 1);
+                }
+            }
+        }
+    }
+}
