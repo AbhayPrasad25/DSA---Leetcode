e358757819f725841e4f7936357741cdaf52f480
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-06-23 17:00:46 +0530

    Time: 0 ms (100%), Space: 45.2 MB (96.24%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,18 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int num = 0;
-        for(int i = 0; i < 32; i++){
-            //iterate over every bit for the every position
-            int count = 0;
-            for(int n : nums){
-                if((n & (1 <<i)) != 0){
-                    count++;
-                }
-            }
-            if(count % 3 != 0){
-                num |= (1 << i);
-            }
+        //Optimal approach based on buckets making buckets of 1 and 2 and storing them 
+        int one = 0, two = 0;
+        for(int n : nums){
+            one = (one ^ n) & ~two;
+            two = (two ^ n) & ~one;
         }
-        return num;
+        return one;
     }
 }
