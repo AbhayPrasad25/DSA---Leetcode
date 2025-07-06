77fef43492b099481827dbd7d8fcebd5eefe5f40
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-06 11:38:35 +0530

    Time: 0 ms (100%), Space: 40.4 MB (65.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,26 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
-        Arrays.fill(memo , -1);
-        return backtrackStep(n , 0, memo);
-    }
-    public int backtrackStep(int n, int step, int[] memo){
-        if(step == n){
-            return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        if(step > n){
-            return 0;
-        }
-        if(memo[step] != -1){
-            return memo[step];
-        }
-        int ways = 0;
-        //we climb with one step at a time
-        ways += backtrackStep(n , step + 1, memo);
-        //next time we climb with 2 step at a time
-        ways += backtrackStep(n , step + 2, memo);
-        //store the number of ways for a given number of stairs
-        memo[step] = ways;
-        return ways;
+        return dp[n];
     }
 }
