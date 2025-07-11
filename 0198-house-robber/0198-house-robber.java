525e41983e9dab8ec69171eb5069a7f3b086b210
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-11 16:41:26 +0530

    Time: 0 ms (100%), Space: 41.4 MB (15.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,8 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
         int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return maxmoney(nums.length - 1, nums, dp);
+        dp[0] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            int take = nums[i];
+            if(i > 1){
+                take += dp[i - 2];
+            }
+            int notTake = dp[i - 1];
+            dp[i] = Math.max(take, notTake);
+        }
+        return dp[nums.length - 1];
     }
     public int maxmoney(int index, int[] nums, int[] dp){
         if(index == 0){
