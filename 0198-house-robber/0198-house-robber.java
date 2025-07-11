1f3e6c6e4f6a825a4540906547c1df6ca7177cfb
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-11 17:14:25 +0530

    Time: 0 ms (100%), Space: 40.9 MB (76.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,30 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
+        int prev1 = nums[0];
+        int prev2 = 0;
         for(int i = 1; i < nums.length; i++){
             int take = nums[i];
             if(i > 1){
-                take += dp[i - 2];
+                take += prev2;
             }
-            int notTake = dp[i - 1];
-            dp[i] = Math.max(take, notTake);
+            int notTake = prev1;
+            int curMax = Math.max(take, notTake);
+            prev2 = prev1;
+            prev1 = curMax;
         }
-        return dp[nums.length - 1];
-    }
-    public int maxmoney(int index, int[] nums, int[] dp){
-        if(index == 0){
-            return nums[index];
-        }
-        if(index < 0){
-            return 0;
-        }
-        if(dp[index] != -1){
-            return dp[index];
-        }
-        int pick = nums[index] + maxmoney(index - 2, nums,dp);
-        int notPick = maxmoney(index - 1, nums,dp);
-        dp[index] = Math.max(pick, notPick);
-        return dp[index];
+        return prev1;
     }
 }
