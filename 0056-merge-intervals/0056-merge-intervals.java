4a4b8dd9225f6893ed6dd1c7a3bb4afd91757588
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-15 21:07:14 +0530

    Time: 8 ms (86.03%), Space: 46.6 MB (56.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -2,20 +2,15 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         //sorting the 2d array based on the starting time
         Arrays.sort(intervals, (a,b) -> Integer.compare(a[0] , b[0]));
-        ArrayList<int[]> mergedInterval = new ArrayList<>();
-        int low = intervals[0][0];
-        int high = intervals[0][1];
-        for(int i = 1; i < intervals.length; i++){
-            if(high < intervals[i][0]){
-                mergedInterval.add(new int[]{low, high});
-                low = intervals[i][0];
-                high = intervals[i][1];
+        LinkedList<int[]> mergedInterval = new LinkedList<>();
+        for(int[] interval : intervals){
+            if(mergedInterval.isEmpty() || mergedInterval.getLast()[1] < interval[0]){
+                mergedInterval.add(interval);
             }
             else{
-                high = Math.max(high, intervals[i][1]);
+                mergedInterval.getLast()[1] = Math.max(mergedInterval.getLast()[1] , interval[1]);
             }
         }
-        mergedInterval.add(new int[]{low, high});
         return mergedInterval.toArray(new int[mergedInterval.size()][]);
     }
 }
