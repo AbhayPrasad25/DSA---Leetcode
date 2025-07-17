1d2181cb51cb79a037bbd1e9eb32d69993711d7d
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-17 15:09:51 +0530

    Time: 3 ms (70.76%), Space: 47.6 MB (55.88%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,5 +1,53 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+        //oprimal space optmization with 1d array
+        int[] dp = new int[grid[0].length];
+        dp[0] = grid[0][0];
+        
+        for(int i = 1; i < grid[0].length; i++){
+            dp[i] = grid[0][i] + dp[i - 1];
+        }
+        for(int i = 1; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(j == 0){
+                    dp[j] = dp[j] + grid[i][j];
+                    continue;
+                }
+                int min = Math.min(dp[j - 1], dp[j]);
+                dp[j] = min + grid[i][j];
+            }
+        }
+
+        return dp[grid[0].length - 1];
+    }
+
+
+    // //Recursion + memoization
+    // public int minPath(int[][] grid, int i , int j, int[][] dp){
+    //     if(i < 0 || j < 0){
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     if(i == 0 && j == 0){
+    //         return grid[i][j];
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     int min = Integer.MAX_VALUE;
+    //     int rightPath = minPath(grid, i-1, j, dp);
+    //     if(rightPath != Integer.MAX_VALUE){
+    //         min = Math.min(min, grid[i][j] + rightPath);
+    //     }
+    //     int downPath = minPath(grid, i , j - 1, dp); 
+    //     if(downPath != Integer.MAX_VALUE){
+    //         min = Math.min(min, grid[i][j] + downPath);
+    //     }
+    //     dp[i][j] = min;
+    //     return dp[i][j]; 
+    // }
+
+
+    /* Tabulation approach an be optimzied further for space
         int[][] dp = new int[grid.length][grid[0].length];
         dp[0][0] = grid[0][0];
         for(int i = 1; i < grid[0].length; i++){
@@ -16,28 +64,5 @@ class Solution {
             }
         }
         return dp[grid.length - 1][grid[0].length - 1];
-    }
-    //Recursion + memoization
-    public int minPath(int[][] grid, int i , int j, int[][] dp){
-        if(i < 0 || j < 0){
-            return Integer.MAX_VALUE;
-        }
-        if(i == 0 && j == 0){
-            return grid[i][j];
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int min = Integer.MAX_VALUE;
-        int rightPath = minPath(grid, i-1, j, dp);
-        if(rightPath != Integer.MAX_VALUE){
-            min = Math.min(min, grid[i][j] + rightPath);
-        }
-        int downPath = minPath(grid, i , j - 1, dp); 
-        if(downPath != Integer.MAX_VALUE){
-            min = Math.min(min, grid[i][j] + downPath);
-        }
-        dp[i][j] = min;
-        return dp[i][j];
-    }
+        */
 }
