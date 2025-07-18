fb325f14c1f4a68aebc0d2dc1877e37c8432bd9d
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-18 16:26:06 +0530

    Time: 0 ms (100%), Space: 42.1 MB (18.43%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -5,33 +5,29 @@ class Solution {
         if(obstacleGrid[0][0] == 1){
             return 0;
         }
-        int[][] dp = new int[m][n];
-        for(int[] d : dp){
-            Arrays.fill(d , 0);
-        }
-        dp[0][0] = 1;
+        int[] dp = new int[n];
+        dp[0] = 1;
         for(int i = 1; i < n; i++){
             if(obstacleGrid[0][i] == 1){
                 break;
             }
-            dp[0][i] += dp[0][i-1];
-        }
-        for(int j = 1; j < m; j++){
-            if(obstacleGrid[j][0] == 1){
-                break;
-            }
-            dp[j][0] += dp[j-1][0];
+            dp[i] += dp[i-1];
         }
 
         for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
+            for(int j = 0; j < n; j++){
                 if(obstacleGrid[i][j] == 1){
-                    dp[i][j] = 0;
+                    dp[j] = 0;
                     continue;
                 }
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+
+                if(j == 0){
+                    continue;
+                }
+                int sum = dp[j] + dp[j-1];
+                dp[j] = sum;
             }
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 }
