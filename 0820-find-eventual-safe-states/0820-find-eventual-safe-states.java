fa696d8bf8b72b3b5a24fa536493450abf75cd45
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-22 15:57:04 +0530

    Time: 26 ms (24.98%), Space: 55.4 MB (74.57%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,23 +1,59 @@
 class Solution {
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-        int V = graph.length;
-        boolean[] checkSafe = new boolean[V];
-        boolean[] visited = new boolean[V];
-        boolean[] pathVisit = new boolean[V];
+    public List<Integer> eventualSafeNodes(int[][] adj) {
+        //DFS Method
+        // int V = graph.length;
+        // boolean[] checkSafe = new boolean[V];
+        // boolean[] visited = new boolean[V];
+        // boolean[] pathVisit = new boolean[V];
 
+        // for(int i = 0; i < V; i++){
+        //     if(!visited[i]){
+        //         dfs(i, graph, visited, checkSafe, pathVisit);
+        //     }
+        // }
+        // List<Integer> safeState = new ArrayList<>();
+        // //now add the safe nodes to the lsit
+        // for(int i = 0; i < V; i++){
+        //     if(checkSafe[i] == true){
+        //         safeState.add(i);
+        //     }
+        // }
+        // return safeState;
+
+        //BFS Method using topo sort
+        int V = adj.length;
+        List<List<Integer>> adjRev = new ArrayList<>();
         for(int i = 0; i < V; i++){
-            if(!visited[i]){
-                dfs(i, graph, visited, checkSafe, pathVisit);
+            adjRev.add(new ArrayList<>());
+        }
+        int[] inDegree = new int[V];
+        for(int i = 0; i < adj.length; i++){
+            for(int node : adj[i]){
+                adjRev.get(node).add(i);
+                inDegree[i]++;
             }
         }
-        List<Integer> safeState = new ArrayList<>();
-        //now add the safe nodes to the lsit
+        //now the edges have been reverse now we will apply topological sorting
+        Queue<Integer> q = new LinkedList<>();
         for(int i = 0; i < V; i++){
-            if(checkSafe[i] == true){
-                safeState.add(i);
+            if(inDegree[i] == 0){
+                q.offer(i);
             }
         }
-        return safeState;
+        List<Integer> safe = new ArrayList<>();
+        while(!q.isEmpty()){
+            int node = q.poll();
+            safe.add(node);
+            for(int adjNode : adjRev.get(node)){
+                inDegree[adjNode]--;
+                //when the inDegree becomes zero add it to the Queue
+                if(inDegree[adjNode] == 0){
+                    q.offer(adjNode);
+                }
+            }
+        }
+        Collections.sort(safe);
+        return safe;
     }
     //dfs traversal to check for safe states
     public boolean dfs(int V, int[][] graph, boolean[] visited, boolean[] safe, boolean[] pathVisit){
