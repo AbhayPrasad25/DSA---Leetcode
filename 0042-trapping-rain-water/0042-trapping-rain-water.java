87b6524707ec9ae689276d561d2ae82594e56b90
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-28 01:27:25 +0530

    Time: 0 ms (100%), Space: 46 MB (81.67%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,39 +1,28 @@
 class Solution {
     public int trap(int[] height) {
-        int[] PGH = pgh(height);
-        int[] NGH = ngh(height);
-        int rainwater = 0;
-        for(int i = 0; i < height.length; i++){
-            rainwater += Math.max(0, Math.min(PGH[i], NGH[i]) - height[i]);
-        }
-        return rainwater;
-    }
-    public static int[] ngh(int[] height){
-        Stack<Integer> stck = new Stack<>();
-        int[] NGH = new int[height.length];
-        int maxh = height[height.length - 1];
-        for(int i = height.length - 1; i >= 0; i--){
-            while(!stck.isEmpty() && stck.peek() < height[i]){
-                stck.pop();
+        int left = 0, maxhL = 0, maxhR = 0;
+        int right = height.length - 1;
+        int trapped = 0;
+        while(left < right){
+            if(height[left] <= height[right]){
+                if(maxhL >= height[left]){
+                    trapped += (maxhL - height[left]);
+                }
+                else{
+                    maxhL = Math.max(maxhL , height[left]);
+                }
+                left++;
             }
-            NGH[i] = stck.isEmpty() ? 0 : stck.peek();
-            maxh = Math.max(maxh, height[i]);
-            stck.push(maxh);
-        }
-        return NGH;
-    }
-    public static int[] pgh(int[] height){
-        Stack<Integer> stck = new Stack<>();
-        int[] PGH = new int[height.length];
-        int maxh = height[0];
-        for(int i = 0; i < height.length; i++){
-            while(!stck.isEmpty() && stck.peek() < height[i]){
-                stck.pop();
+            else{
+                if(maxhR >= height[right]){
+                    trapped += (maxhR - height[right]);
+                }
+                else{
+                    maxhR = Math.max(maxhR, height[right]);
+                }
+                right--;
             }
-            PGH[i] = stck.isEmpty() ? 0 : stck.peek();
-            maxh = Math.max(maxh, height[i]);
-            stck.push(maxh);
         }
-        return PGH;
+        return trapped;
     }
 }
