cfda430d4bd3d98fc15e62e795db3585635627c6
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-29 01:09:38 +0530

    Time: 290 ms (5.03%), Space: 45.2 MB (50.47%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,16 +1,25 @@
 class Solution {
     public int jump(int[] nums) {
         int[] dp = new int[nums.length];
-        Arrays.fill(dp , -1);
-        return minJump(0, nums, dp);
+        int n = nums.length;
+        //atleast we will do one jump from the firstIndex
+        dp[0] = 0;
+        for(int i = 1; i < nums.length; i++){
+            //all possible jumps we can do from each index
+            int min = n;
+            for(int j = 0; j < i; j++){
+                if(j + nums[j] >= i){
+                    min = Math.min(min , dp[j] + 1);
+                }
+            }
+            dp[i] = min;
+        }
+        return dp[n - 1];
     }
     //recursive approach
     public int minJump(int index , int[] nums, int[] dp){
         if(index >= nums.length -1){
             return 0;
-        }
-        if(dp[index] != -1){
-            return dp[index];
         }
         int min = nums.length;
         for(int i = 1; i <= nums[index]; i++){
