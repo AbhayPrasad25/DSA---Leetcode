3a06dad67b0a5e3ee3ea08cf16239c0e978c411d
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-07-29 01:30:56 +0530

    Time: 1 ms (99.48%), Space: 45.3 MB (41.09%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,33 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] dp = new int[nums.length];
-        int n = nums.length;
-        //atleast we will do one jump from the firstIndex
-        dp[0] = 0;
-        for(int i = 1; i < nums.length; i++){
-            //all possible jumps we can do from each index
-            int min = n;
-            for(int j = 0; j < i; j++){
-                if(j + nums[j] >= i){
-                    min = Math.min(min , dp[j] + 1);
-                }
+        int jump = 0,l = 0, r = 0;
+        while(r < nums.length - 1){
+            int farthest = 0;
+            for(int i = l; i <= r; i++){
+                farthest = Math.max(i + nums[i], farthest);
             }
-            dp[i] = min;
+            l = r + 1;
+            r = farthest;
+            jump++;
         }
-        return dp[n - 1];
-    }
-    //recursive approach
-    public int minJump(int index , int[] nums, int[] dp){
-        if(index >= nums.length -1){
-            return 0;
-        }
-        int min = nums.length;
-        for(int i = 1; i <= nums[index]; i++){
-            if(index + i < nums.length){
-                min = Math.min(min , 1 + minJump(index + i, nums, dp));
-            }
-        }
-        dp[index] = min;
-        return dp[index];
+        return jump;
     }
 }
