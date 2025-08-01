fa784968967e7e159ecd1a38b5c6537ff6e0327d
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-08-01 10:54:24 +0530

    Time: 28 ms (24.52%), Space: 45.8 MB (10.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,28 +1,33 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] memo = new int[amount + 1];
-        Arrays.fill(memo , -1);
-        int res = dfs(coins, amount,memo);
-        return (res == Integer.MAX_VALUE ? -1 : res);
+        int[][] dp = new int[coins.length][amount + 1];
+        for(int[] dp1 : dp){
+            Arrays.fill(dp1, -1);
+        }
+        int num = minCoins(coins.length - 1, coins, amount, dp);
+        if(num >= Integer.MAX_VALUE - 1){
+            return -1;
+        }
+        return num;
     }
-    public int dfs(int[] coins, int amount, int[] memo){
-        if(amount == 0){
-            return 0;
-        }
-        if(amount > 0 && memo[amount] != -1){
-            return memo[amount];
-        }
-        if(amount < 0){
-            return Integer.MAX_VALUE;
-        }
-        int ans = Integer.MAX_VALUE;
-        for(int coin : coins){
-            int res = dfs(coins, amount - coin, memo);
-            if(res == Integer.MAX_VALUE){
-                continue;
+    public int minCoins(int index, int[] coins, int amount, int[][] dp){
+        if(index == 0){
+            if(amount % coins[0] == 0){
+                return amount / coins[0];
             }
-            ans = Math.min(ans, res + 1);
+            else{
+                return Integer.MAX_VALUE - 1;
+            }
         }
-        return memo[amount] = ans;
+        if(dp[index][amount] != -1 && amount > 0){
+            return dp[index][amount];
+        }
+        int take = Integer.MAX_VALUE - 1;
+        if(coins[index] <= amount){
+            take = 1 + minCoins(index, coins, amount - coins[index], dp);
+        }
+        int notTake = minCoins(index - 1, coins, amount, dp);
+        dp[index][amount] = Math.min(take, notTake);
+        return  dp[index][amount];
     }
 }
