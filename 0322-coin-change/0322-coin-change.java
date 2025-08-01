fe3a013baa0be2b8246d9fbeac07bea1b893a7aa
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-08-01 19:25:50 +0530

    Time: 15 ms (76.49%), Space: 45.3 MB (21.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,14 +1,29 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        //tabulation
         int[][] dp = new int[coins.length][amount + 1];
-        for(int[] dp1 : dp){
-            Arrays.fill(dp1, -1);
+        for(int i = 0; i <= amount; i++){
+            if(i % coins[0] == 0){
+                dp[0][i] = i / coins[0];
+            }
+            else{
+                dp[0][i] = (int)1e9;
+            }
         }
-        int num = minCoins(coins.length - 1, coins, amount, dp);
-        if(num >= Integer.MAX_VALUE - 1){
+        for(int i = 1; i < coins.length; i++){
+            for(int j = 0; j <= amount; j++){
+                int take = (int)1e9;
+                if(coins[i] <= j){
+                    take = 1 + dp[i][j - coins[i]];
+                }
+                int notTake = dp[i - 1][j];
+                dp[i][j] = Math.min(take, notTake);
+            }
+        }
+        if(dp[coins.length - 1][amount] >= (int)1e9){
             return -1;
         }
-        return num;
+        return dp[coins.length - 1][amount];
     }
     public int minCoins(int index, int[] coins, int amount, int[][] dp){
         if(index == 0){
