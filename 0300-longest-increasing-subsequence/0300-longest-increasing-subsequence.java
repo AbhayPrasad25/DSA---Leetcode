fb861bc85d2b256f4210fe4937b5bbfe3ef19426
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-08-02 22:47:38 +0530

    Time: 5 ms (92.85%), Space: 44.5 MB (48.36%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,32 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp = new int[nums.length][nums.length];
-        for(int[] dp1 : dp){
-            Arrays.fill(dp1 , -1);
+        List<Integer> lis = new ArrayList<>();
+        lis.add(nums[0]);
+        int len = 1;
+        for(int i = 0; i < nums.length; i++){
+            if(lis.get(lis.size() - 1) < nums[i]){
+                lis.add(nums[i]);
+                len++;
+            }
+            else{
+                int Index = binarySearch(lis, nums[i]);
+                lis.set(Index, nums[i]);
+            }
         }
-        return recurseLis(0, nums, -1, dp);
+        return len;
     }
-    public int recurseLis(int index, int[] nums, int prev, int[][] dp){
-        if(index == nums.length){
-            return 0;
+    public static int binarySearch(List<Integer> lis, int el){
+        int left = 0;
+        int right = lis.size() - 1;
+        int index = 0;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(lis.get(mid) >= el){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
         }
-        if(dp[index][prev + 1] != -1){
-            return dp[index][prev + 1];
-        }
-        int take = Integer.MIN_VALUE;
-        if(prev == -1 || nums[index] > nums[prev]){
-            take = 1 + recurseLis(index + 1, nums, index, dp);
-        }
-        int notTake = recurseLis(index + 1, nums, prev, dp);
-        dp[index][prev + 1] = Math.max(take, notTake);
-        return dp[index][prev + 1];
+        return left;
     }
 }
