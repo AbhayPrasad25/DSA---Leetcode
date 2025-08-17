88f16befa20bec43fe37d4ecfc8bf0ba13443696
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-08-17 21:43:32 +0530

    Time: 0 ms (100%), Space: 45.7 MB (72.21%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,23 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        int i = 0;
-        int lMax = prices[0];
-        int lMin = prices[0];
+        int aheadBuy = 0, aheadNotBuy = 0;
         int n = prices.length;
-        while(i < n - 1){
-            // when the left value is greater than the right we have to not pick that vakue
-            while(i < n - 1 && prices[i] >= prices[i + 1]){
-                i++;
-            }
-            lMin = prices[i];
-            // Pick the value which is smaller than the next
-            while(i < n - 1 && prices[i] <= prices[i + 1]){
-                i++;
-            }
-            lMax = prices[i];
-            profit += lMax - lMin;
+        for(int i = n - 1; i >= 0; i--){
+            int currBuy = Math.max(aheadBuy, prices[i] + aheadNotBuy);
+            int currNotBuy = Math.max(aheadNotBuy, -prices[i] + aheadBuy);
+            aheadBuy = currBuy;
+            aheadNotBuy = currNotBuy;
         }
-        return profit;
+        return aheadNotBuy;
     }
 }
